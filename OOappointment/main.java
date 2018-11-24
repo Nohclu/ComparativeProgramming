81eb2596ddd806f6aa3ec3ddd7ff603cd3249c06
Connor Mulready <connor.mulready2@mail.dcu.ie>
2018-11-24 14:55:46 +0000

    Infinite User input, Swap Array to Map

--- OOappointment/main.java
+++ OOappointment/main.java
@@ -1,42 +1,55 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Day[] week = {new Day("mon"),
-                        new Day("tue"),
-                        new Day("wed"),
-                        new Day("thu"),
-                        new Day("fri"),
-                        new Day("sat"),
-                        new Day("sun")};
-        
-        for(Day day: week){
-            System.out.println(day.getName());
-            System.out.println(day.getAppointments());;
-        };
+        Map<String, Day> week = new HashMap<String, Day>();
+        week.put("mon", new Day("mon"));
+        week.put("tue", new Day("tue"));
+        week.put("wed", new Day("wed"));
+        week.put("thu", new Day("thu"));
+        week.put("fri", new Day("fri"));
+        week.put("sat", new Day("sat"));
+        week.put("sun", new Day("sun"));
 
-        Appointment t = new Appointment("Doc","00:30","12:00");
-        week[2].append(t);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Loop Start");
+        while (true){
+            String input = scan.nextLine();
+           
+            if (input.equals("add")){
+                System.out.println("Enter: day name start finish ex. mon dentist 08:43 14:30");
+                String newAppDet = scan.nextLine();
+                String[] userInput = newAppDet.split(" ");
+                for ( String key : week.keySet()) {
+                    if (key.equals(userInput[0])){
+                        Day day = week.get(key);
+                        day.append(new Appointment(userInput[1],userInput[2],userInput[3]));
+                        break;
+                    }
+                }
+            }
+            if (input.equals("show")){
+                System.out.println("Enter: day ex. tue");
+                String userInput = scan.nextLine();
+                for ( String key : week.keySet()) {
+                    if (key.equals(userInput)){
+                        Day day = week.get(key);
+                        System.out.println(day.getAppointments());
+                        for (Appointment app: day.appointments){
+                            System.out.println(app.getName() +" "+app.getStartTime()+" "+app.getFinishTime());
+                        }
+                        break;
+                    }
+                }
+            }
 
-        for(Day day: week){
-            System.out.println(day.getName());
-            for(Appointment app: day.appointments){
-                System.out.println(app.getName());
-                System.out.println(app.getStartTime());
-                System.out.println(app.getFinishTime());
-            };
-        };
+            if (input.equals("quit")){
+                break;
+            }
 
-        Appointment z = new Appointment("Doc","00:35","12:00");
-        week[2].append(z);
-
-        for(Day day: week){
-            System.out.println(day.getName());
-            for(Appointment app: day.appointments){
-                System.out.println(app.getName());
-                System.out.println(app.getStartTime());
-                System.out.println(app.getFinishTime());
-            };
-        };
+        }
     }
 }
